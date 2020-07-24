e27f82b9a40454127e99362e46502c0bad465c1d
Melvin Schmidt <melvin.schmidt92@googlemail.com>
2020-07-24 10:05:01 +0200

    add diagrams

--- app/views/LocationOverviewView.js
+++ app/views/LocationOverviewView.js
@@ -1,23 +1,74 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import JSONTree from 'react-native-json-tree'
-
+import Dimensions from 'react-dimensions'
+import {
+  BarChart,
+} from "react-native-chart-kit";
 function LocationOverviewView(props) {
-  if (typeof props.locations != "undefined" && props.locations.size === 0) {
-    return null;
+  let locationsData = props.locations.toJS();
+  let locations = [];
+  for (const [key, value] of Object.entries(locationsData)) {
+    let { popularTimes, ...locationData } = value;
+    let location = {
+      ...locationData, chart: {
+        labels: ['Mon.', 'Dien.', 'Mittw.', 'Donn.', 'Freit.', 'Sam.', 'Sonn.'],
+        datasets: [{
+          data: [sum(popularTimes.monday),
+          sum(popularTimes.tuesday),
+          sum(popularTimes.wednesday),
+          sum(popularTimes.thursday),
+          sum(popularTimes.friday),
+          sum(popularTimes.saturday),
+          sum(popularTimes.sunday),
+          ]
+        }
+        ]
+      }
+    }
+    locations.push(location);
+
   }
-  return(
-    <View style={styles.container}>
-          <JSONTree data={props.locations} />
-      </View>
- 
-  ) 
-  return null;
- }
- const styles = StyleSheet.create({
+  console.log(locations);
+
+  function sum(arr) {
+    var sum = 0;
+    for (var index = 0; index < arr.length; index++) {
+      sum += arr[index];
+    }
+    return sum;
+  }
+
+  const locationItems = locations.map((location) =>
+    <View>
+      <Text>{location.id}</Text>
+      <Text>{location.name}</Text>
+      <Text>{location.address}</Text>
+      <BarChart
+        data={location.chart}
+        width={350} // from react-native
+        height={220}
+        chartConfig={{
+          backgroundColor: '#ffffff',
+          backgroundGradientFrom: '#ffffff',
+          backgroundGradientTo: '#ffffff',
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          decimalPlaces: 0
+        }}
+
+      />
+    </View>
+  );
+
+
+  return (
+    <ScrollView>{locationItems}</ScrollView>
+  )
+}
+const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    paddingTop: 22
   },
   item: {
     padding: 10,
